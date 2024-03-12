a99bdb75618d3d21962336916c0a4d9f35126284
daniel endaylalu <endaylaludaniel@gmail.com>
2024-03-12 16:35:11 +0300

    added media query for 480px

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,36 @@
 // import React from "react";
 
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
 function Navbar() {
   const navitems = ["Home", "Apps", "Explore", "LeaderShip", "Join"];
+  const [navShow, setNavShow] = useState(false);
   return (
     <div className="nav-bar">
       <div className="logo">
         <h2 className="logo">
-          <a href="#">Ship On</a>
+          <a href="#" onClick={() => setNavShow(false)}>
+            Ship On
+          </a>
         </h2>
       </div>
       <div className="nav">
-        <ul className="nav-items">
+        <FontAwesomeIcon
+          icon={faBars}
+          className="nav-menu"
+          onClick={() => setNavShow(!navShow)}
+        />
+        <ul className={`nav-items ${navShow && "nav-show"}`}>
           {navitems.map((item) => {
             return (
               <li className="nav-item" key={item}>
-                <a href="#" className="nav-link">
+                <a
+                  href="#"
+                  className="nav-link"
+                  onClick={() => setNavShow(false)}
+                >
                   {item}
                 </a>
               </li>
